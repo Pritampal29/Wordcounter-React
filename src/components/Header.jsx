e33acc0c23275c6d6b1f3c1add9ece2e2bbc0d57
Pritam Pal <dmepritam@gmail.com>
2025-04-06 20:13:58 +0530

    Update Header.jsx
    change mobile offcanvas menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Header = () => {
+  const handleCloseOffcanvas = () => {
+    const offcanvasEl = document.getElementById("mobileMenu");
+    const bsOffcanvas = window.bootstrap?.Offcanvas.getInstance(offcanvasEl);
+    bsOffcanvas?.hide();
+  };
+
   return (
     <header className="header bg-light text-dark">
       <div className="container d-flex justify-content-between align-items-center py-2">
@@ -10,7 +16,7 @@ const Header = () => {
           <h2 className="m-0">PRITAM PAL</h2>
         </Link>
 
-        {/* Toggle button for mobile */}
+        {/* Mobile Toggle Button */}
         <button
           className="btn d-md-none"
           type="button"
@@ -21,7 +27,7 @@ const Header = () => {
           <i className="bi bi-list" style={{ fontSize: "1.5rem" }}></i>
         </button>
 
-        {/* Desktop Nav */}
+        {/* Desktop Navigation */}
         <nav className="d-none d-md-block">
           <ul className="nav">
             <li className="nav-item">
@@ -52,9 +58,9 @@ const Header = () => {
           </ul>
         </nav>
 
-        {/* Mode Switch */}
+        {/* Desktop Mode Switch */}
         <div className="form-check form-switch d-none d-md-block">
-          <label style={{ marginTop: "4px" }} htmlFor="modeChecker">
+          <label htmlFor="modeChecker" style={{ marginTop: "4px" }}>
             Switch Mode
           </label>
           <input
@@ -66,7 +72,7 @@ const Header = () => {
         </div>
       </div>
 
-      {/* Offcanvas Menu for Mobile */}
+      {/* Mobile Offcanvas Menu */}
       <div
         className="offcanvas offcanvas-end"
         tabIndex="-1"
@@ -87,43 +93,27 @@ const Header = () => {
         <div className="offcanvas-body">
           <ul className="nav flex-column">
             <li className="nav-item">
-              <Link to="/" className="nav-link" data-bs-dismiss="offcanvas">
+              <Link to="/" className="nav-link" onClick={handleCloseOffcanvas}>
                 Home
               </Link>
             </li>
             <li className="nav-item">
-              <Link
-                to="/about"
-                className="nav-link"
-                data-bs-dismiss="offcanvas"
-              >
+              <Link to="/about" className="nav-link" onClick={handleCloseOffcanvas}>
                 About
               </Link>
             </li>
             <li className="nav-item">
-              <Link
-                to="/todos"
-                className="nav-link"
-                data-bs-dismiss="offcanvas"
-              >
+              <Link to="/todos" className="nav-link" onClick={handleCloseOffcanvas}>
                 Todo App
               </Link>
             </li>
             <li className="nav-item">
-              <Link
-                to="/word-counter"
-                className="nav-link"
-                data-bs-dismiss="offcanvas"
-              >
+              <Link to="/word-counter" className="nav-link" onClick={handleCloseOffcanvas}>
                 Word Counter
               </Link>
             </li>
             <li className="nav-item">
-              <Link
-                to="/courses"
-                className="nav-link"
-                data-bs-dismiss="offcanvas"
-              >
+              <Link to="/courses" className="nav-link" onClick={handleCloseOffcanvas}>
                 Courses
               </Link>
             </li>
